bc2b701a0d5b311f2df8d504c694c3e03a87cc4d
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2018-05-09 18:56:10 +0200

    Add automatic deployment

---
+++ ci/github-pages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TEMP=$(mktemp -d)
+REV=$(git rev-parse --short HEAD)
+GIT="git -C $TEMP"
+
+set -ex
+
+cp -R spec/book/* $TEMP/
+cp CNAME _config.yml $TEMP/
+
+$GIT init
+
+$GIT config user.name "Ink the Squid"
+$GIT config user.name "ink@squid-lang.org"
+
+$GIT remote add upstream "https://$GH_TOKEN@github.com/squid-lang/spec.git"
+$GIT fetch upstream
+$GIT reset upstream/gh-pages
+
+$GIT add -A
+$GIT commit -m "Build for $REV"
+$GIT push -q upstream HEAD:gh-pages
